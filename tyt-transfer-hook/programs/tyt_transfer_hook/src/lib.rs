96d75a328e0495101fadeb73d81a7aca16597fdd
FincelDesign <kevin@finceldesign.com>
2025-04-15 02:29:39 +0000

    Sync Anchor version to 0.31.0 and remove separate solana-program dependency

--- tyt-transfer-hook/programs/tyt_transfer_hook/src/lib.rs
+++ tyt-transfer-hook/programs/tyt_transfer_hook/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("C3Hj4rA8FXPBVP8kfNKujEXJiFsHZgaFGhaEo6iVSWw8");
+declare_id!("GeRJJx7idjqg2Z4sbtbupJG34AszA9gCLn4Jxd5G7ChM");
 
 #[program]
 pub mod tyt_transfer_hook {
