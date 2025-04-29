dd7db665fd60fb84f96ff71cba720f0656e573a6
FincelDesign <kevin@finceldesign.com>
2025-04-29 01:10:03 +0000

    Fixed solana devnet setup on install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,9 +11,6 @@ solana --version
 cargo install spl-token-cli --locked --force
 spl-token --version
 
-# Update Solana CLI config (optional)
-solana config set --url http://localhost:8899
-
 # Restore Solana Wallet from GitHub Secrets
 mkdir -p ~/.config/solana
 echo "$SOLANA_DEVNET_WALLET" > ~/.config/solana/id.json
